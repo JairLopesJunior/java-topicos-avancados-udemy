4adbcc57d0d96aa5566b1e21819cd2e48573c63e
unknown <jair.lopes@fatec.sp.gov.br>
2023-01-10 14:02:33 -0300

    Sistema de Módulos (Projeto Jigsaw)

---
+++ topicosavancados/src/main/java/aula16/com/mod/goodbye/GoodBye.java
@@ -0,0 +1,8 @@
+package aula16.com.mod.goodbye;
+
+public class GoodBye {
+
+    public void diga() {
+        System.out.println("Até logo!");
+    }
+}
---
+++ topicosavancados/src/main/java/aula16/com/mod/hello/HelloWorldJigsaw.java
@@ -0,0 +1,10 @@
+package aula16.com.mod.hello;
+
+public class HelloWorldJigsaw {
+
+    public void diga() {
+        System.out.println("Olá Mundo!");
+    }
+}
+
+
