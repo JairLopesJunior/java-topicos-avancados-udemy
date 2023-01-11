0ad3df3dc3291033ef8761f5bbef211b56957cb4
unknown <jair.lopes@fatec.sp.gov.br>
2023-01-11 10:29:07 -0300

    HTTP/2

---
+++ topicosavancados/src/main/java/aula23Http2/Exemplo.java
@@ -0,0 +1,13 @@
+package aula23Http2;
+
+
+import java.net.http.HttpClient;
+
+public class Exemplo {
+
+    public static void main(String[] args) {
+
+        HttpClient client = HttpClient.newHttpClient();
+
+    }
+}
