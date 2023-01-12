a91f5c214af81ba44556974d89c7e5a047e0b47a
unknown <jair.lopes@fatec.sp.gov.br>
2023-01-12 09:24:14 -0300

    Classe Files - Método mismatch

---
+++ topicosavancados/src/main/java/aula36MetodoMismatch/Exemplo.java
@@ -0,0 +1,17 @@
+package aula36MetodoMismatch;
+
+import java.io.File;
+import java.nio.file.Files;
+
+public class Exemplo {
+
+    // JAVA 12
+    public static void main(String[] args) {
+
+        File f1 = new File("texto1.txt");
+        File f2 = new File("texto2.txt");
+
+        long resultado = Files.mismatch(f1.toPath(), f2.toPath());
+        System.out.println(resultado);
+    }
+}
