ce100987a20d6ca641296a1c9eebf3637d3a0466
unknown <jair.lopes@fatec.sp.gov.br>
2023-01-12 10:53:39 -0300

    Text Blocks

---
+++ topicosavancados/src/main/java/aula41TextBlocks/Exemplo.java
@@ -0,0 +1,14 @@
+package aula41TextBlocks;
+
+public class Exemplo {
+
+    // JAVA 13
+    public static void main(String[] args) {
+
+        String texto = """ Olá Mundo!
+                           Este é o módulo
+                           Java 13 """;
+
+        System.out.println(texto);
+    }
+}
